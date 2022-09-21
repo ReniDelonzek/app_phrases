3823e6010487a8e00258c79cf92f2444bbd145b1
Reni Delonzek <renidelonzek@gmail.com>
2022-09-21 00:00:18 -0300

    upd: url firebase

--- lib/data/datasources/remote_datasource.dart
+++ lib/data/datasources/remote_datasource.dart
@@ -10,12 +10,15 @@ abstract class RemoteDatasource {
 class RemoteDatasourceImpl implements RemoteDatasource {
   String get url => kDebugMode
       ? 'http://localhost:5001/project-1126612155927744763/us-central1/'
-      : '';
+      // TODO preencher com o URL do projeto
+      // Ele vai estar no formato de https://us-central1-${Código do projeto}.cloudfunctions.net/
+
+      : 'https://us-central1-project-1126612155927744763.cloudfunctions.net/';
 
   @override
   Future<bool> updateTokenByUser(String token) async {
     try {
-      var response = await Dio().post(url + 'updateUserToken',
+      var response = await Dio().post('$url/updateUserToken',
           options: Options(headers: {'Authorization': 'Bearer $token'}));
       return response.statusCode == 200 && response.data['success'];
     } catch (error) {
